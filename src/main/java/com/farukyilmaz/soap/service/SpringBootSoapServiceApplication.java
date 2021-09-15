7fb474d08a02019fcf51bd7cf8916d1565986340
farukyilmazgithub <90024205+farukyilmazgithub@users.noreply.github.com>
2021-09-15 15:29:06 +0300

    Update SpringBootSoapServiceApplication.java

--- src/main/java/com/farukyilmaz/soap/service/SpringBootSoapServiceApplication.java
+++ src/main/java/com/farukyilmaz/soap/service/SpringBootSoapServiceApplication.java
@@ -3,6 +3,8 @@ package com.farukyilmaz.soap.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// farukyilmazgithub 15.09.2021
+
 @SpringBootApplication
 public class SpringBootSoapServiceApplication {
 	public static void main(String[] args) { SpringApplication.run(SpringBootSoapServiceApplication.class, args); }
